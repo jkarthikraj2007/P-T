f68169761c31dcd1b313ef130f1143cb060eb140
J Karthik Raj <rajkarthik622@gmail.com>
2026-09-24 12:29:42 +0530

    commit

--- printarray.c
+++ printarray.c
@@ -4,13 +4,13 @@ int main()
     int n;
     printf("Enter number of elements of array a: ");
     scanf("%d",&n);
-int a[n];
-printf("Enter %d elements of array a: ",n);
+    int a[n];
+    printf("Enter %d elements of array a: ",n);
     for(int i=0; i<n; i++)
     {
         scanf("%d",&a[i]);
     }
-printf("Elements of array a are: ");
+    printf("Elements of array a are: ");
     for(int i=0; i<n; i++)
     {
         printf("%d ",a[i]);
@@ -35,5 +35,15 @@ printf("Elements of array a are: ");
     }
     printf("\nMaximum element of array a is: %d",max);
     printf("\nMinimum element of array a is: %d",min);
+
+    int sum=0;
+    for(int i=0; i<n; i++)
+    {
+        sum+=a[i];
+    }
+    float avg=0;
+    avg=sum/n;
+    printf("\nSum of elements of array a is: %d",sum);
+    printf("\nAverage of elements of array a is: %.2f",avg);
     return 0;
 }
